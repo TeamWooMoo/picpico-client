383f078b832dfd9a49728c110e3402666d4eb7ae
choidabom <dabomi0305@naver.com>
2023-01-15 21:59:17 +0900

    [FEAT] Room.js 버튼 수정

--- picpico-client/src/components/Form/EnterRoomForm.js
+++ picpico-client/src/components/Form/EnterRoomForm.js
@@ -16,18 +16,18 @@ function EnterRoomForm() {
     console.log(event);
     axios
       .get(API.ROOM + roomId)
-      .then((res) => {
+      .then(res => {
         const roomName = res.data.roomId;
         navigate(`/room/${roomName}`);
       })
-      .catch((error) => {
+      .catch(error => {
         alert("해당 코드의 방이 존재하지 않습니다.");
       });
   }
   return (
     <Form onSubmit={onEnterRoomFormSubmit}>
-      <Form.ControlLabel>Enter room Code</Form.ControlLabel>
-      <Input placeholder="room code" onChange={onRoomInputChange} />
+      <Form.ControlLabel style={{ padding: "5px" }}>방 코드를 입력하세요.</Form.ControlLabel>
+      <Input placeholder="Room Code" onChange={onRoomInputChange} style={{ borderRadius: "20px", margin: "5px 0" }} />
     </Form>
   );
 }
