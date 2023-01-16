dcec3b26f45dc52fec45b9b97eeb21322625e739
wnhlee <2wheeh@gmail.com>
2023-01-16 18:56:20 +0900

    [FIX]config 배포용 서버로 BASE_URL 수정

--- picpico-client/src/config.js
+++ picpico-client/src/config.js
@@ -1,5 +1,5 @@
-//export const BASE_URL = "https://picpico-server.site";
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://picpico-server.site";
+// export const BASE_URL = "http://localhost:3000";
 
 export const API = {
   ROOM: `${BASE_URL}/rooms/`,
