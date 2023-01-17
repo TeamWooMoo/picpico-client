78679445cbaeab1ec2adf226d92ac7b4ff855dd3
midnightkang <106954289+midnightkang@users.noreply.github.com>
2023-01-18 04:48:01 +0900

    [REFATOR] javascriptSDK_key update

--- picpico-client/src/config.js
+++ picpico-client/src/config.js
@@ -2,7 +2,7 @@ export const BASE_URL = "https://picpico-server.site";
 // export const BASE_URL = "https://jaehyeonkim.shop";
 // export const BASE_URL = "http://localhost:3000";
 
-export const KakaoShareKey = "842b33b67472bc4c3fdce2d9c25a509f";
+export const KakaoShareKey = "8bd30d3aeaf34785e2f9aa979e51715f";
 
 export const API = {
   ROOM: `${BASE_URL}/rooms/`,
