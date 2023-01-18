52f061cf046629d19ce0cbcea14ea9b8638d0fc9
midnightkang <106954289+midnightkang@users.noreply.github.com>
2023-01-19 01:51:06 +0900

    [FEAT] 닉네임 받아오기 구현

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { join } from "path";
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
-  
+
   app.useStaticAssets(join(__dirname, "..", "picpico-client/build"));
   app.setBaseViewsDir(join(__dirname, "..", "picpico-client/build"));
 
@@ -14,7 +14,7 @@ async function bootstrap() {
   app.setViewEngine("html");
 
   const handleListen = () => console.log("Client Server");
-  // await app.listen(3000, handleListen);
-  await app.listen(3001, handleListen);
+  await app.listen(3000, handleListen);
+  // await app.listen(3001, handleListen);
 }
 bootstrap();
