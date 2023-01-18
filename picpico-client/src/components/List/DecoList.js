c6d9c30d8aeb59536081c0cb7ef851d438e6347b
narin <nalynnshinhwa1128@gmail.com>
2023-01-19 05:49:29 +0900

    [FEAT] decolist 수정 중

--- picpico-client/src/components/List/DecoList.js
+++ picpico-client/src/components/List/DecoList.js
@@ -9,6 +9,7 @@ const DecoList = () => {
   const [prevIdx, setPrevIdx] = useState(1);
   const dispatch = useDispatch();
   const decoData = useSelector(state => state.decoInfo.decoList);
+  const idxArr = Object.keys(decoData);
   // /*
   //   사진 인덱스  {
   //     viewers: [{nickName: 닉네임, socketId: 닉네임}]
@@ -25,9 +26,9 @@ const DecoList = () => {
   return (
     <>
       <ImageList sx={{ width: 350, height: 350 }} cols={3} rowHeight={150}>
-        {Object.values(decoData).map(({ picture }, idx) => (
+        {idxArr.map(idx => (
           <ImageListItem style={{ flex: 1, flexDirection: "row" }}>
-            <img alt={`deco-${idx + 1}`} onClick={onDecoImgClick} src={picture} data-deco={idx + 1} id={`deco-${idx + 1}`} />
+            <img alt={`deco-${idx + 1}`} onClick={onDecoImgClick} src={decoData[idx]["picture"]} data-deco={idx + 1} id={`deco-${idx + 1}`} />
           </ImageListItem>
         ))}
       </ImageList>
