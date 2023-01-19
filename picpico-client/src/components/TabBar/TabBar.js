637892e0e4217aca5119b887ee19cef0eb860725
narin <nalynnshinhwa1128@gmail.com>
2023-01-19 13:02:26 +0900

    [FEAT] 발표 자료_2

--- picpico-client/src/components/TabBar/TabBar.js
+++ picpico-client/src/components/TabBar/TabBar.js
@@ -1,7 +1,7 @@
-import {useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
-import {setDecoModeInfo} from "../../slice/decoInfo";
+import { setDecoModeInfo } from "../../slice/decoInfo";
 import ColorList from "../List/ColorList";
 import StickerList from "../List/StickerList";
 import DecoDragAndDrop from "../../modules/decoDragAndDrop.mjs";
@@ -50,52 +50,50 @@ const TabMenu = styled.div`
 `;
 
 const Desc = styled.div`
-    text-align: center;
-    aligin-items: center;
+  text-align: center;
+  aligin-items: center;
 `;
 
 const TabBar = () => {
-    const targetImgIdx = useSelector(state => state.decoInfo.myDecoCanvas);
-    const dispatch = useDispatch();
-    // Tab Menu 중 현재 어떤 Tab이 선택되어 있는지 확인하기 위한 currentTab 상태와 currentTab을 갱신하는 함수가 존재해야 하고, 초기값은 0.
-    const [currentTab, clickTab] = useState(0);
+  const targetImgIdx = useSelector(state => state.decoInfo.myDecoCanvas);
+  const dispatch = useDispatch();
+  // Tab Menu 중 현재 어떤 Tab이 선택되어 있는지 확인하기 위한 currentTab 상태와 currentTab을 갱신하는 함수가 존재해야 하고, 초기값은 0.
+  const [currentTab, clickTab] = useState(0);
 
-    const menuArr = [
-        {name: "Color", content: <ColorList />},
-        {name: "Sticker", content: <StickerList />},
-    ];
+  const menuArr = [
+    { name: "Color", content: <ColorList /> },
+    { name: "Sticker", content: <StickerList /> },
+  ];
 
-    const dragAndDrop = DecoDragAndDrop();
+  const dragAndDrop = DecoDragAndDrop();
 
-    const selectMenuHandler = index => {
-        // parameter로 현재 선택한 인덱스 값을 전달해야 하며, 이벤트 객체(event)는 쓰지 않는다
-        // 해당 함수가 실행되면 현재 선택된 Tab Menu 가 갱신.
-        clickTab(index);
-        if (index === 0) {
-            dragAndDrop.reset();
-            dispatch(setDecoModeInfo({value: "stroke"}));
-        } else {
-            dragAndDrop.init(targetImgIdx);
-            dispatch(setDecoModeInfo({value: "sticker"}));
-        }
-    };
+  const selectMenuHandler = index => {
+    clickTab(index);
+    if (index === 0) {
+      dragAndDrop.reset();
+      dispatch(setDecoModeInfo({ value: "stroke" }));
+    } else {
+      dragAndDrop.init(targetImgIdx);
+      dispatch(setDecoModeInfo({ value: "sticker" }));
+    }
+  };
 
-    return (
-        <>
-            <div>
-                <TabMenu>
-                    {menuArr.map((el, index) => (
-                        <li className={index === currentTab ? "submenu focused" : "submenu"} onClick={() => selectMenuHandler(index)}>
-                            {el.name}
-                        </li>
-                    ))}
-                </TabMenu>
-                <Desc>
-                    <p>{menuArr[currentTab].content}</p>
-                </Desc>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <TabMenu>
+          {menuArr.map((el, index) => (
+            <li className={index === currentTab ? "submenu focused" : "submenu"} onClick={() => selectMenuHandler(index)}>
+              {el.name}
+            </li>
+          ))}
+        </TabMenu>
+        <Desc>
+          <p>{menuArr[currentTab].content}</p>
+        </Desc>
+      </div>
+    </>
+  );
 };
 
 export default TabBar;
