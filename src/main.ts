33ef60d82fa95350a635dc608a18484e8f90d78e
narin <nalynnshinhwa1128@gmail.com>
2023-01-19 12:53:48 +0900

    [FEAT] 발표 자료_1

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
-import {NestFactory} from "@nestjs/core";
-import {AppModule} from "./app.module";
-import {NestExpressApplication} from "@nestjs/platform-express";
-import {join} from "path";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "path";
 
 async function bootstrap() {
-    const app = await NestFactory.create<NestExpressApplication>(AppModule);
-    app.enableCors();
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
 
-    app.useStaticAssets(join(__dirname, "..", "picpico-client/build"));
-    app.setBaseViewsDir(join(__dirname, "..", "picpico-client/build"));
+  app.useStaticAssets(join(__dirname, "..", "picpico-client/build"));
+  app.setBaseViewsDir(join(__dirname, "..", "picpico-client/build"));
 
-    app.engine("html", require("ejs").renderFile);
-    app.setViewEngine("html");
+  app.engine("html", require("ejs").renderFile);
+  app.setViewEngine("html");
 
-    const handleListen = () => console.log("Client Server");
-    // await app.listen(3000, handleListen);
-    await app.listen(3001, handleListen);
+  const handleListen = () => console.log("Client Server");
+  await app.listen(3000, handleListen);
+  // await app.listen(3001, handleListen);
 }
 bootstrap();
