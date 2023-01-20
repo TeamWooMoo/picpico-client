ecd3bfbf66c005802bfd382af2e019bc06dee487
narin <nalynnshinhwa1128@gmail.com>
2023-01-20 18:09:05 +0900

    [TEST] emit test

--- picpico-client/src/modules/decoCanvas.mjs
+++ picpico-client/src/modules/decoCanvas.mjs
@@ -1,13 +1,11 @@
-import {setDecoListInfo, setStickerInfo} from "../slice/decoInfo.js";
+import { setDecoListInfo, setStickerInfo } from "../slice/decoInfo.js";
 import store from "../store.js";
 
 export const onPickDecoEvent = obj => {
-    console.log("obj from server", obj);
-    store.dispatch(setDecoListInfo({value: obj}));
+  console.log("obj from server", obj);
+  store.dispatch(setDecoListInfo({ value: obj }));
 };
 
 export const onPickStickerEvent = (url, imgIdx) => {
-    store.dispatch(setStickerInfo({value: {idx: imgIdx, url: url}}));
+  store.dispatch(setStickerInfo({ value: { idx: imgIdx, url: url } }));
 };
-
-export const onStickerMoveEvent = () => {};
