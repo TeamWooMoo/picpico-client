ce0d555a5abd5f270de32f2e1fbf7d992856834e
narin <nalynnshinhwa1128@gmail.com>
2023-01-21 23:39:30 +0900

    [FEAT] my face index 지정 시점 수정

--- picpico-client/src/modules/resetMember.mjs
+++ picpico-client/src/modules/resetMember.mjs
@@ -3,16 +3,17 @@ import { setMembersInfo } from "../slice/membersInfo.js";
 import { socket } from "./sockets.mjs";
 //key: picturenumber, value: object_key: viewers , value: []
 export const onResetMemberEvent = nicknameArr => {
-    console.log("reset member on");
+    console.log("reset member on", nicknameArr);
     store.dispatch(setMembersInfo({ value: nicknameArr }));
     let myOrder;
     for (let i = 0; i < nicknameArr.length - 1; i++) {
         if (nicknameArr[i].socketId === socket.id) {
             myOrder = i;
-            console.log("my order!!!!");
+            console.log("my order!!!!", i);
             break;
         }
     }
+    console.log("myOrder", myOrder);
     const myFace = document.getElementById("myFace");
     myFace.style.zIndex = myOrder;
 };
