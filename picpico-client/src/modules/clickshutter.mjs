be05135313a2107491ebd2b0e4eb5ed077d10aeb
narin <nalynnshinhwa1128@gmail.com>
2023-01-21 15:24:21 +0900

    [TEST] 웹 워커 테스트 일시 중지

--- picpico-client/src/modules/clickshutter.mjs
+++ picpico-client/src/modules/clickshutter.mjs
@@ -1,8 +1,6 @@
 import { setIdxCount, setTakePic } from "../slice/takepicInfo.js";
 import store from "../store.js";
 import { socket } from "./sockets.mjs";
-import Webworker from "../workers/Webworker.js";
-import { myWorker } from "../workers/myWorker.js";
 export const onClickShutterEvent = idx => {
   console.log("애들아 찍어!");
   store.dispatch(setIdxCount({ value: (parseInt(idx) + 1).toString() }));
@@ -11,35 +9,24 @@ export const onClickShutterEvent = idx => {
 
 export const onSendPicEvent = async (idx, imgArr) => {
   const canvas = document.getElementById("drawnCanvas");
-  // const ctx = canvas.getContext("2d");
-  // async function first() {
-  //   await imgArr.forEach(async obj => {
-  //     const url = obj.picture;
-  //     const img = new Image();
-  //     img.src = url;
-  //     img.onload = async function () {
-  //       await ctx.drawImage(img, 0, 0);
-  //     };
-  //   });
-  // }
-  // await first();
-  const _worker = new Webworker(myWorker);
-  _worker.onmessage = msg => console.log(">>>", msg);
+  const ctx = canvas.getContext("2d");
+  async function first() {
+    await imgArr.forEach(async obj => {
+      const url = obj.picture;
+      const img = new Image();
+      img.src = url;
+      img.onload = async function () {
+        await ctx.drawImage(img, 0, 0);
+      };
+    });
+  }
+  await first();
+  // const _worker = new Webworker(myWorker);
+  // _worker.onmessage = msg => console.log(">>>", msg);
 
   const resultUrl = await canvas.toDataURL("image/png");
   socket.emit("result_pic", idx, resultUrl);
   console.log("result:", resultUrl);
   console.log("사진 5장 다 그렸음. 서버야 이게 최종본이야");
-  // ctx.clearRect(0, 0, 350, 350);
-  // if (window.Worker) {
-  //   // if (worker) {
-  //   //   stopWorker();
-  //   // }
-  //   worker = new Worker("./worker.js");
-  //   worker.postMessage("워커 실행");
-  //   worker.onmessage = function (e) {
-  //     console.log("호출 페이지 - ", e.data);
-  //     alert(e.data);
-  //   };
-  // }
+  ctx.clearRect(0, 0, 350, 350);
 };
