43940a4db6dd9a521b975da08b1b83cbeb4e686d
wnhlee <2wheeh@gmail.com>
2023-01-22 00:49:12 +0900

    [FEAT]isMuted 이미 전 페이지에 적용 확인 불필요한 코드 삭제

--- picpico-client/src/components/Btn/MuteBtn.js
+++ picpico-client/src/components/Btn/MuteBtn.js
@@ -1,18 +1,22 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { handleMuteClick } from "../../modules/stream.mjs";
+import { setMutedInfo } from "../../slice/picpicoInfo.js";
 import mic from "./../../assets/images/icon-mic.png";
 import mute from "./../../assets/images/icon-mute.png";
 
 function MuteBtn() {
-    const [muted, setMuted] = useState(true);
+    const dispatch = useDispatch();
+    const isMuted = useSelector(state => state.picpicoInfo.isMuted);
+
     const onMuteOnOff = () => {
-        setMuted(current => !current);
-        handleMuteClick(muted);
+        handleMuteClick(isMuted);
+        dispatch(setMutedInfo({ value: !isMuted }));
         console.log("mute on off");
     };
     return (
         <>
-            {muted ? (
+            {isMuted ? (
                 <img src={mic} className="drop-shadow" style={{ width: "40px", height: "40px" }} onClick={onMuteOnOff} />
             ) : (
                 <img src={mute} className="drop-shadow" style={{ width: "40px", height: "40px" }} onClick={onMuteOnOff} />
