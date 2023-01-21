da997b91657e45b61adb22c8968ff0377af6a85c
narin <nalynnshinhwa1128@gmail.com>
2023-01-21 22:36:00 +0900

    [STYLE] memberArr 구조 확인

--- picpico-client/src/modules/resetMember.mjs
+++ picpico-client/src/modules/resetMember.mjs
@@ -4,10 +4,12 @@ import { socket } from "./sockets.mjs";
 //key: picturenumber, value: object_key: viewers , value: []
 export const onResetMemberEvent = nicknameArr => {
     const memberArr = store.getState().membersInfo.members;
+    console.log("memberArr", memberArr);
     let myOrder;
     for (let i = 0; i < memberArr.length - 1; i++) {
         if (memberArr[i].socketId === socket.id) {
             myOrder = i;
+            console.log("my order!!!!");
             break;
         }
     }
