64ffff9ccd808fb13d43113174819ff2763d3f37
narin <nalynnshinhwa1128@gmail.com>
2023-01-22 22:07:25 +0900

    [FEAT] 0번째 사진부터 보이게 수정

--- picpico-client/src/components/List/SelectList.js
+++ picpico-client/src/components/List/SelectList.js
@@ -43,12 +43,12 @@ const SelectList = () => {
     return (
         <>
             <ImageList sx={{ justifyContent: "center", width: 350, height: 500, borderRadius: "7px" }} cols={1} rowHeight={350}>
-                {Object.values(imgData).map(({ picture }, idx) => (
+                {imgData.map((url, idx) => (
                     <ImageListItem>
                         <img
                             alt={`pic-${idx}`}
                             onClick={onImageClick}
-                            src={picture}
+                            src={url}
                             data-pid={idx}
                             id={`pic-${idx}`}
                             style={{ backgroundColor: "white" }}
