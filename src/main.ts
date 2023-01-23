d856913c766cf15ecd1a30114d96eb80c10d1576
narin <nalynnshinhwa1128@gmail.com>
2023-01-23 15:39:44 +0900

    [ETC] config 수정

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import {NestFactory} from "@nestjs/core";
-import {AppModule} from "./app.module";
-import {NestExpressApplication} from "@nestjs/platform-express";
-import {join} from "path";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "path";
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -14,7 +14,7 @@ async function bootstrap() {
     app.setViewEngine("html");
 
     const handleListen = () => console.log("Client Server");
-    // await app.listen(3000, handleListen);
-    await app.listen(3001, handleListen);
+    await app.listen(3000, handleListen);
+    // await app.listen(3001, handleListen);
 }
 bootstrap();
