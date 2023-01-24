40edb9197af5f92728fdc376af654a19b8599f72
narin <nalynnshinhwa1128@gmail.com>
2023-01-24 13:46:43 +0900

    [FEAT] result 생성 후 결과 캔버스 보이도록 수정

--- picpico-client/src/routes/Gallery.js
+++ picpico-client/src/routes/Gallery.js
@@ -1,9 +1,9 @@
-import {useEffect} from "react";
-import {useSelector} from "react-redux";
-import {Container} from "rsuite";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Container } from "rsuite";
 import PicDownloadBtn from "../components/Btn/PicDownloadBtn";
 import GalleryDoneBtn from "../components/Btn/GalleryDoneBtn";
-import {gifTest} from "../test/resultTest.mjs";
+import { gifTest } from "../test/resultTest.mjs";
 
 const Gallery = () => {
     // 아래의 div에 들어갈 것
@@ -11,7 +11,7 @@ const Gallery = () => {
 
     useEffect(() => {
         gifTest(result);
-    }, []);
+    }, [result]);
     return (
         <Container>
             <PicDownloadBtn />
