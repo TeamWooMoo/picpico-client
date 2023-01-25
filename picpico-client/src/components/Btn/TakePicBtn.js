51a15eb351262aff89e10724eb3430c4fb1262a2
narin <nalynnshinhwa1128@gmail.com>
2023-01-26 01:14:35 +0900

    [FEAT] shutter click 모바일에서 1번만 소리나도록 수정

--- picpico-client/src/components/Btn/TakePicBtn.js
+++ picpico-client/src/components/Btn/TakePicBtn.js
@@ -2,7 +2,6 @@ import { useSelector } from "react-redux";
 import { IoRadioButtonOnOutline } from "react-icons/io5";
 import { socket } from "../../modules/sockets.mjs";
 import shutterSound from "./../../assets/sound/shutter.mp3";
-import { isMobile } from "react-device-detect";
 
 function TakePicBtn() {
     const idx = useSelector(state => state.takepicInfo.idx);
@@ -10,19 +9,12 @@ function TakePicBtn() {
     const shuttersound = new Audio(shutterSound);
 
     const onTakePicBtnTouch = e => {
-        // e.preventDefault();
+        e.preventDefault();
+    };
+    const onTakePicBtnClick = e => {
         shuttersound.play().catch(e => {
             console.log(e);
         });
-    };
-    const onTakePicBtnClick = e => {
-        if (!isMobile) {
-            console.log("사진 찍히니 ~");
-            d;
-            shuttersound.play().catch(e => {
-                console.log(e);
-            });
-        }
 
         socket.emit("click_shutter", idx);
     };
