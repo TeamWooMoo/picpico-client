2e776723755b9dd0bdea874efc56fafa87204cdc
choidabom <dabomi0305@naver.com>
2023-01-25 20:15:13 +0900

    [FEAT] add background none

--- picpico-client/src/components/List/BackgroundList.js
+++ picpico-client/src/components/List/BackgroundList.js
@@ -1,28 +1,17 @@
-import { useDispatch, useSelector } from "react-redux";
 import { FlexboxGrid } from "rsuite";
 import star from "./../../assets/images/background-star.png";
 import water from "./../../assets/images/background-water.png";
-import { socket } from "../../modules/sockets.mjs";
-import { v1 as uuid } from "uuid";
+import none from "./../../assets/images/background-none.png";
 
 const StickerList = () => {
-    const dispatch = useDispatch();
-    const targetImgIdx = useSelector(state => state.decoInfo.myDecoCanvas);
-    const onStickerClick = url => {
-        console.log("background");
-        // const newStickerId = uuid();
-        // console.log("newStickerId>>", newStickerId);
-        // socket.emit("pick_sticker", url, targetImgIdx, newStickerId);
-        // dispatch(setDecoModeInfo({ value: "sticker" }));
-    };
-
     return (
         <>
             <FlexboxGrid justify="center" className="color-options">
                 <div style={{ height: "40px" }}></div>
                 <div id="">
-                    <img style={{ height: "40px", margin: "0 5px" }} src={star} draggable="true" />
-                    <img style={{ height: "40px", margin: "0 5px" }} src={water} draggable="true" />
+                    <img style={{ height: "40px", margin: "0 5px", borderRadius: "7px" }} src={none} draggable="true" />
+                    <img style={{ height: "40px", margin: "0 5px", borderRadius: "7px" }} src={star} draggable="true" />
+                    <img style={{ height: "40px", margin: "0 5px", borderRadius: "7px" }} src={water} draggable="true" />
                 </div>
             </FlexboxGrid>
         </>
