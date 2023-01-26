ef3a32eb85851182a49aed4545cb29ba53bec8bd
narin <nalynnshinhwa1128@gmail.com>
2023-01-26 23:16:30 +0900

    [FEAT] background 추가

--- picpico-client/src/components/Canvas/DecoCanvas.js
+++ picpico-client/src/components/Canvas/DecoCanvas.js
@@ -7,10 +7,11 @@ import { FlexboxGrid } from "rsuite";
 import { setDecoModeInfo, setResultInfo } from "../../slice/decoInfo";
 import { ResultImage, Sticker } from "../../modules/resultCanvas.mjs";
 import { isMobile } from "react-device-detect";
-import star from "../../assets/images/background-star.png";
-import water from "../../assets/images/background-water.png";
+import snow from "./../../assets/images/background-snow.png";
+import boom from "./../../assets/images/background-boom.png";
+import party from "./../../assets/images/background-party.png";
+import water from "./../../assets/images/background-water.png";
 import matrix from "./../../assets/images/background-matrix.png";
-
 const DecoCanvas = () => {
     const dispatch = useDispatch();
     const stickerList = useSelector(state => state.decoInfo.stickerList);
@@ -25,7 +26,7 @@ const DecoCanvas = () => {
     const strokeColor = useSelector(state => state.drawingInfo.strokeColor);
     const stickerPointList = useSelector(state => state.decoInfo.stickerPointList);
     const bgList = useSelector(state => state.decoInfo.bgList);
-    const bgSrcList = ["", star, water, matrix];
+    const bgSrcList = ["", snow, party, boom, water, matrix];
 
     const decoColors = useSelector(state => state.decoInfo.colorList);
     const decoMapping = {};
