edd078ffae831ec322f2a0ba1f7e2900e9678f9d
narin <nalynnshinhwa1128@gmail.com>
2023-01-26 09:41:58 +0900

    [FEAT] 직접 만든스티커만 리스트에 넣는 버전으로 수정

--- picpico-client/src/components/List/StickerList.js
+++ picpico-client/src/components/List/StickerList.js
@@ -4,9 +4,6 @@ import { FlexboxGrid } from "rsuite";
 import sticker1 from "../../assets/gif/sticker1.gif";
 import sticker2 from "../../assets/gif/sticker2.gif";
 import sticker3 from "../../assets/gif/sticker3.gif";
-import coding from "../../assets/gif/coding.gif";
-import happy from "../../assets/gif/happy.gif";
-import heart from "../../assets/gif/heart.gif";
 
 import { socket } from "../../modules/sockets.mjs";
 import { setDecoModeInfo } from "../../slice/decoInfo";
@@ -28,12 +25,9 @@ const StickerList = () => {
             <FlexboxGrid justify="center" className="color-options">
                 <div style={{ height: "40px" }}></div>
                 <div id="">
-                    <img style={{ height: "40px" }} onClick={() => onStickerClick(sticker1)} src={sticker1} draggable="true" />
-                    <img style={{ height: "40px" }} onClick={() => onStickerClick(happy)} src={happy} draggable="true" />
-                    <img style={{ height: "40px" }} onClick={() => onStickerClick(sticker2)} src={sticker2} draggable="true" />
-                    <img style={{ height: "40px" }} onClick={() => onStickerClick(coding)} src={coding} draggable="true" />
-                    <img style={{ height: "40px" }} onClick={() => onStickerClick(sticker3)} src={sticker3} draggable="true" />
-                    <img style={{ height: "40px" }} onClick={() => onStickerClick(heart)} src={heart} draggable="true" />
+                    <img style={{ height: "40px" }} onClick={() => onStickerClick(sticker1)} src={sticker1} draggable="true" alt="sticker1" />
+                    <img style={{ height: "40px" }} onClick={() => onStickerClick(sticker2)} src={sticker2} draggable="true" alt="sticker2" />
+                    <img style={{ height: "40px" }} onClick={() => onStickerClick(sticker3)} src={sticker3} draggable="true" alt="sticker3" />
                 </div>
             </FlexboxGrid>
         </>
